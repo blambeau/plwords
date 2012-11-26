b0dbe0fe1dce59f82987751c72afdf4e334f8962
Bernard Lambeau <blambeau@gmail.com>
2012-11-26 11:45:34 +0100

    Debug.

--- app.rb
+++ app.rb
@@ -3,7 +3,13 @@ require 'sequel'
 require 'thread'
 
 LOCK = Mutex.new
-DB   = Sequel.connect(ENV['DATABASE_URL'])
+#DB   = Sequel.connect(ENV['DATABASE_URL'])
+
+get '/' do
+  status 200
+  content_type "text/plain"
+  ENV['DATABASE_URL']
+end
 
 get '/status' do
   status 200
--- config.ru
+++ config.ru
@@ -1,2 +1,2 @@
-require File.expand_path('../app', __FILE__)
+require './app'
 run Sinatra::Application
