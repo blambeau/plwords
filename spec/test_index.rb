44bd2e65d2b4d6b9c944a4f2fd03b6ca246596ab
Bernard Lambeau <blambeau@gmail.com>
2012-11-29 07:47:34 +0100

    Add test for the index page.

---
+++ spec/test_index.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe "GET /" do
+
+  before do
+    get '/'
+  end
+
+  it 'displays the html layout' do
+    last_response.body.should =~ /Programming Language Word Clouds/ 
+  end
+
+  it 'displays the form' do
+    last_response.body.should =~ /<form/ 
+  end
+
+end
