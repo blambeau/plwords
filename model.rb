b54cab595645b27efe040b90001016591365ffed
Bernard Lambeau <blambeau@gmail.com>
2012-11-29 13:37:02 +0100

    Factorize submission cleaning.

--- model.rb
+++ model.rb
@@ -4,14 +4,25 @@ module Model
 
   native :submissions
 
+  def cleaned_submissions
+    extend(
+      submissions,
+      language: ->{ language.downcase })
+  end
+
+  def languages
+    summarize(
+      cleaned_submissions,
+      [:language],
+      submission_count: count())
+  end
+
   def wordcloud_by_language
     s = self
     allbut(
       extend(
         summarize(
-          extend(
-            submissions,
-            language: ->{ language.downcase }),
+          cleaned_submissions,
           [:language],
           feeling: concat(between: "\n"){ feeling },
           submission_count: count()),
