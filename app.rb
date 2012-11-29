c9eb9e44906f5dbeadc12303a28736dc5c85f482
Bernard Lambeau <blambeau@gmail.com>
2012-11-29 08:52:25 +0100

    Rename submissions#tags -> submissions#feeling

--- app.rb
+++ app.rb
@@ -23,8 +23,9 @@ end
 post '/submissions' do
   halt(400, 'language') unless lang=params['language']
   halt(400, 'language') if lang.empty? or lang.size>50
-  halt(400, 'tags')     unless tags=params['tags']
-  halt(400, 'tags')     if tags.empty? or tags.size>500
-  relvar(:submissions).insert(language: lang, tags: tags, submission_ip: request.ip)
+  halt(400, 'feeling')  unless feeling=params['feeling']
+  halt(400, 'feeling')  if feeling.empty? or feeling.size>500
+  lang, feeling = lang.strip, feeling.strip
+  relvar(:submissions).insert(language: lang, feeling: feeling, submission_ip: request.ip)
   201
 end
--- spec/test_post_submissions.rb
+++ spec/test_post_submissions.rb
@@ -16,7 +16,7 @@ describe "POST /submissions" do
   ]
 
   it 'posts successfully with valid params' do
-    post '/submissions', language: "java", tags: JAVA_WORDS.join("\n")
+    post '/submissions', language: "java", feeling: JAVA_WORDS.join("\n")
     last_response.status.should eq(201)
   end
 
@@ -26,35 +26,35 @@ describe "POST /submissions" do
 
     post '/submissions', language: "java"
     last_response.status.should eq(400)
-    last_response.body.should eq("tags")
+    last_response.body.should eq("feeling")
 
-    post '/submissions', tags: "enterprise"
+    post '/submissions', feeling: "enterprise"
     last_response.status.should eq(400)
     last_response.body.should eq("language")
   end
 
   it 'detects empty language' do
-    post '/submissions', language: "", tags: "blah"
+    post '/submissions', language: "", feeling: "blah"
     last_response.status.should eq(400)
     last_response.body.should eq("language")
   end
 
-  it 'detects empty tags' do
-    post '/submissions', language: "ruby", tags: ""
+  it 'detects empty feeling' do
+    post '/submissions', language: "ruby", feeling: ""
     last_response.status.should eq(400)
-    last_response.body.should eq("tags")
+    last_response.body.should eq("feeling")
   end
 
   it 'detects empty language too long' do
-    post '/submissions', language: "A"*1000, tags: "blah"
+    post '/submissions', language: "A"*1000, feeling: "blah"
     last_response.status.should eq(400)
     last_response.body.should eq("language")
   end
 
-  it 'detects empty tags too long' do
-    post '/submissions', language: "ruby", tags: "A"*1000
+  it 'detects empty feeling too long' do
+    post '/submissions', language: "ruby", feeling: "A"*1000
     last_response.status.should eq(400)
-    last_response.body.should eq("tags")
+    last_response.body.should eq("feeling")
   end
 
 end
