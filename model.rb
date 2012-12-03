a963b38009565323545dc3e9d88ed57142280cfb
Bernard Lambeau <blambeau@gmail.com>
2012-12-03 11:17:57 +0100

    Banish words.

--- model.rb
+++ model.rb
@@ -4,11 +4,13 @@ module Model
   def histogram(text)
     histo = text.split(/\W+/)
                 .map(&:downcase)
+                .reject{|w| w.size==1}
                 .each_with_object(Hash.new 0){|w,h| h[w] += 1 }
     Relation histo.each_pair.map{|w,f| {word: w, frequency: f}}
   end
 
   native :submissions
+  native :banished_words
 
   def languages
     summarize(
@@ -19,14 +21,20 @@ module Model
 
   def wordcloud_by_language
     s = self
-    allbut(
-      extend(
-        summarize(
-          submissions,
-          [:language],
-          feeling: concat(between: "\n"){ feeling },
-          submission_count: count()),
-        histogram: ->{ s.histogram(feeling) }),
-      [:feeling])
+    group(
+      not_matching(
+        ungroup(
+          allbut(
+            extend(
+              summarize(
+                submissions,
+                [:language],
+              feeling: concat(between: "\n"){ feeling },
+              submission_count: count()),
+            histogram: ->{ s.histogram(feeling) }),
+          [:feeling]),
+        :histogram),
+      banished_words),
+    [:word, :frequency], :histogram)
   end
 end
